65cbe40010fd6a9e9555a451e526cda4b02fb048
itsbib <itsbibekarki@gmail.com>
2023-01-14 23:43:24 +0545

    merge error solved

--- backends/types/user.type.ts
+++ backends/types/user.type.ts
@@ -1,13 +1,6 @@
 export type userType = {
-<<<<<<< HEAD
   fullName: string;
   email: string;
   password: string;
   age: number;
-=======
-    fullName: string;
-    email: string;
-    password: string;
-    age: number;
->>>>>>> 39fd81640e8369cb357021e3cdda25cfe148cd0b
 };
