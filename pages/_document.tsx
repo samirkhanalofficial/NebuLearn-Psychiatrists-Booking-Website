0e762efb4c180d8d798494bb9c088f6219e214cf
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-14 19:38:05 +0545

    removed next experimental

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
-import { Montserrat } from "@next/font/google";
-const montserrat = Montserrat({ subsets: ["latin"] });
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={montserrat.className}>
+      <body>
         <Main />
         <NextScript />
       </body>
