02c68930ddda347ca1c7e55b8edbb8d39ff54628
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-14 19:35:48 +0545

    removed next experimental

---
+++ components/HeroSection.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { ReactNode } from "react";
+import Image from "next/image";
+import style from "../styles/HeroSection.module.css";
+export default function HeroSection(props: {
+  firstLine: string;
+  secondLine: string;
+  subTitle: ReactNode;
+  src: string;
+  firstWord: string;
+  lastWord: string;
+}) {
+  return (
+    <>
+      <div className={style.mainForm}>
+        <div className={style.TextContent}>
+          <h1 className={style.firstHeading}>
+            <span className={style.blueWord}>{props.firstWord}</span>{" "}
+            {props.firstLine}
+          </h1>
+          <h1 className={style.firstHeading}>
+            {props.secondLine}
+            <span className={style.blueWord}> {props.lastWord}</span>
+          </h1>
+          <p className={style.subTitle}>{props.subTitle}</p>
+          <div>
+            <button className={style.button}>Get Started</button>
+          </div>
+        </div>
+        <div className={style.imageContent}>
+          <Image
+            src={props.src}
+            alt="header content"
+            width={500}
+            height={300}
+            className={style.photo}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
