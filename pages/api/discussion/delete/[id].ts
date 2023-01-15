3ac4999b14416fc973155eea9de69904d6feb3aa
aavashb <baralaavas@gmail.com>
2023-01-15 06:54:50 +0545

    Delete and add discussion

---
+++ pages/api/discussion/delete/[id].ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { discussionController } from "@/backends/controllers/discussion.controller";
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method == "DELETE")
+        return discussionController.deleteDiscussion(req, res);
+    else
+        return res.status(400).json({
+            message: "METHOD NOT ALLOWED",
+        });
+}
