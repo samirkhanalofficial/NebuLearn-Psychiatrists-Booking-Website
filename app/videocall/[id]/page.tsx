8583e5be23cb8fa83988e8acbc4a402c25744e46
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-19 16:26:52 +0545

    fuck you nextjs

--- app/videocall/[id]/page.tsx
+++ app/videocall/[id]/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({ params, searchParams }: any) {
     id: params.id,
     token: searchParams.token,
   });
-  console.log("search params: " + searchParams);
+  console.log("search params: " + searchParams.json());
   return (
     <VideoCallScreen
       meetingId={data.meetingId}
