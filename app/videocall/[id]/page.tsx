6cb0d8d67083b0264ae416d708dc1e20a29cb447
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-19 16:40:35 +0545

    fuck you nextjs

--- app/videocall/[id]/page.tsx
+++ app/videocall/[id]/page.tsx
@@ -4,36 +4,36 @@ import dynamic from "next/dynamic";
 const VideoCallScreen = dynamic(() => import("./VideoCallScreen"), {
   ssr: false,
 });
-async function getUserDetails(params: any) {
-  const { id, token } = params;
-  console.log(params);
-  const res = await fetch("https://codestorm.samirk.com.np/api/videocall", {
-    method: "POST",
-    body: JSON.stringify({
-      id: id,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      authorization: token || "",
-    },
-  });
-  if (res.status != 200) return redirect("/");
-  const data = await res.json();
-  return data;
-}
+// async function getUserDetails(params: any) {
+//   const { id, token } = params;
+//   const res = await fetch("https://codestorm.samirk.com.np/api/videocall", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       id: id,
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//       authorization: token || "",
+//     },
+//   });
+//   if (res.status != 200) return redirect("/");
+//   const data = await res.json();
+//   return data;
+// }
 export default async function Page({ params, searchParams }: any) {
   console.log("search params: " + searchParams);
-  const data = await getUserDetails({
-    id: params.id,
-    token: searchParams.token,
-  });
-  console.log("search params: " + searchParams.get("token"));
+  // const data = await getUserDetails({
+  //   id: params.id,
+  //   token: searchParams.token,
+  // });
+  console.log("search params: " + searchParams.token);
   return (
-    <VideoCallScreen
-      meetingId={data.meetingId}
-      partnerId={data.partnerId}
-      myId={data.myId}
-      meeting={data.meeting}
-    />
+    // <VideoCallScreen
+    //   meetingId={data.meetingId}
+    //   partnerId={data.partnerId}
+    //   myId={data.myId}
+    //   meeting={data.meeting}
+    // />
+    <>testing</>
   );
 }
