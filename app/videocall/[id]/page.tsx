1419287874c2ac4f8c13031c64d65d21c3af7a43
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-19 16:46:10 +0545

    fuck you nextjs

--- app/videocall/[id]/page.tsx
+++ app/videocall/[id]/page.tsx
@@ -4,29 +4,29 @@ import dynamic from "next/dynamic";
 const VideoCallScreen = dynamic(() => import("./VideoCallScreen"), {
   ssr: false,
 });
-// async function getUserDetails(params: any) {
-//   const { id, token } = params;
-//   const res = await fetch("https://codestorm.samirk.com.np/api/videocall", {
-//     method: "POST",
-//     body: JSON.stringify({
-//       id: id,
-//     }),
-//     headers: {
-//       "Content-Type": "application/json",
-//       authorization: token || "",
-//     },
-//   });
-//   if (res.status != 200) return redirect("/");
-//   const data = await res.json();
-//   return data;
-// }
+async function getUserDetails(params: any) {
+  console.log("params:" + params);
+  //   const { id, token } = params;
+  //   const res = await fetch("https://codestorm.samirk.com.np/api/videocall", {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       id: id,
+  //     }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       authorization: token || "",
+  //     },
+  //   });
+  //   if (res.status != 200) return redirect("/");
+  //   const data = await res.json();
+  //   return data;
+}
 export default async function Page({ params, searchParams }: any) {
   console.log("search params: " + searchParams);
-  // const data = await getUserDetails({
-  //   id: params.id,
-  //   token: searchParams.token,
-  // });
-  console.log("search params: " + searchParams.token);
+  const data = await getUserDetails({
+    id: params.id,
+    token: searchParams.token,
+  });
   return (
     // <VideoCallScreen
     //   meetingId={data.meetingId}
