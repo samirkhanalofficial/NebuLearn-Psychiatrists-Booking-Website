720a02d71b950c807ad1691a5e78f1869714c5af
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-19 14:54:17 +0545

    video call testing

--- app/videocall/[id]/[token]/page.tsx
+++ app/videocall/[id]/[token]/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 import React from "react";
 import VideoCallScreen from "./VideoCallScreen";
 async function getUserDetails({ params }: any) {
-  // http://localhost:3000/videocall/testid/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzhlN2EwZWVhZjU1MTNkNDBkYmM0MyIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsImxvZ2luQXQiOiJUaHUgSmFuIDE5IDIwMjMgMTI6MzM6MTMgR01UKzA1NDUgKE5lcGFsIFRpbWUpIiwicm9sZSI6InVzZXIiLCJpYXQiOjE2NzQxMTA4OTMsImV4cCI6MTY3NDQ3MDg5M30.yhnzQv5FXXtgV45uwnJ4GcftcfOsPAr9PX0k1axWtb8
   const { id, token } = params;
   const res = await fetch("http://localhost:3000/api/videocall", {
     method: "POST",
@@ -25,12 +24,5 @@ export default async function Page({
   params: { id: string; token: string };
 }) {
   const data = await getUserDetails({ params });
-  return (
-    <VideoCallScreen
-      meetingId={data.meetingId}
-      partnerId={data.partnerId}
-      myId={data.myId}
-      meeting={data.meeting}
-    />
-  );
+  return <>hello</>;
 }
