7a696697e551e7da32b59fcd7933d9bd99a3b50c
samirkhanalofficial <samirkhanalofficial@gmail.com>
2023-01-19 14:43:39 +0545

    video call testing

--- app/videocall/[id]/[token]/page.tsx
+++ app/videocall/[id]/[token]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import React from "react";
 import VideoCallScreen from "./VideoCallScreen";
-export async function getUserDetails({ params }: any) {
+async function getUserDetails({ params }: any) {
   // http://localhost:3000/videocall/testid/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzhlN2EwZWVhZjU1MTNkNDBkYmM0MyIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsImxvZ2luQXQiOiJUaHUgSmFuIDE5IDIwMjMgMTI6MzM6MTMgR01UKzA1NDUgKE5lcGFsIFRpbWUpIiwicm9sZSI6InVzZXIiLCJpYXQiOjE2NzQxMTA4OTMsImV4cCI6MTY3NDQ3MDg5M30.yhnzQv5FXXtgV45uwnJ4GcftcfOsPAr9PX0k1axWtb8
   const { id, token } = params.params;
   const res = await fetch("http://localhost:3000/api/videocall", {
